5ac73d64d0c1e4d77fefe20e39bf801824c0d6e1
icebear0111 <wltjq0901@naver.com>
2025-04-04 17:34:26 +0900

    project setup

---
+++ src/main/java/com/owlibrary/OwlibraryApplication.java
@@ -0,0 +1,13 @@
+package com.owlibrary;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OwlibraryApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OwlibraryApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/owlibrary/OwlibraryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.owlibrary;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OwlibraryApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
